841a620e19bc9a32780a28e569cb200a9c93e3f1
Ben Pang <benjinpang@gmail.com>
2019-10-08 23:48:47 +0400

    add nav + title

--- js/main.js
+++ js/main.js
@@ -4,32 +4,47 @@ let f_init = _=>{
     licenseKey: '0BD90318-FEBE4A0A-AB35E65A-31E4D65D',
     sectionSelector:'.full',
     verticalCentered: false,
-    scrollingSpeed: 1000
+    scrollingSpeed: 1000,
+    onLeave: (a,b)=>{
+      n_up(b.index)
+    }
   })
 }
 
-$(_=>{
+// change active square in nav
+let n_up = n=>{
+  $('.nav').text('□')
+  $('.nav').eq(n).text('■')
+}
 
-  // handles recursive action in last panel
-  // turns off fullpage.js temporarily to allow DOM changes
-  let handler = _=>{
-    let panes = $('.cont > .full')
-    let hand = $('.scaled')
-    let cpane = $('.scaled').parents('.full').clone()
+// handles recursive action in last panel
+// turns off fullpage.js temporarily to allow DOM changes
+let handler = _=>{
+  let panes = $('.cont > .full')
+  let hand = $('.scaled')
+  let cpane = $('.scaled').parents('.full').clone()
 
-    $.fn.fullpage.destroy('all')
+  $.fn.fullpage.destroy('all')
 
-    panes.first().remove()
-    hand.first().removeClass('scaled').off('click')
-    cpane.removeClass('active')
-    hand.addClass('active')
-    $('.cont').append(panes.slice(1, -1), cpane)
-
-    f_init()
-    $('.scaled').click(handler)
-  }
+  panes.first().remove()
+  hand.first().removeClass('scaled').off('click')
+  cpane.removeClass('active')
+  hand.addClass('active')
+  $('.cont').append(panes.slice(1, -1), cpane)
+  n_up(0)
 
   f_init()
+  $('.scaled').click(handler)
+}
+
+$(_=>{
+  f_init()
+  n_up(0)
   $('body').removeClass('faded')
   $('.scaled').click(handler)
+  $('.nav').click(e=>{
+    let ind = $(e.target).index()
+    n_up(ind)
+    $.fn.fullpage.moveTo(ind + 1)
+  })
 })
