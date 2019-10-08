9bfff5b54105cbf64b90b476f3ea7c9549040af2
Ben Pang <benjinpang@gmail.com>
2019-10-08 23:14:49 +0400

    add draft version of brains

--- js/main.js
+++ js/main.js
@@ -1,14 +1,35 @@
+// initialize fullpage.js
+let f_init = _=>{
+  $('.cont').fullpage({
+    licenseKey: '0BD90318-FEBE4A0A-AB35E65A-31E4D65D',
+    sectionSelector:'.full',
+    verticalCentered: false,
+    scrollingSpeed: 1000
+  })
+}
+
 $(_=>{
+
+  // handles recursive action in last panel
+  // turns off fullpage.js temporarily to allow DOM changes
   let handler = _=>{
-    let panes = $('body > .full')
+    let panes = $('.cont > .full')
     let hand = $('.scaled')
-    let cpane = $('.scaled').parent().clone()
+    let cpane = $('.scaled').parents('.full').clone()
+
+    $.fn.fullpage.destroy('all')
 
     panes.first().remove()
     hand.first().removeClass('scaled').off('click')
-    $('body').append(panes.slice(1, -1), cpane)
+    cpane.removeClass('active')
+    hand.addClass('active')
+    $('.cont').append(panes.slice(1, -1), cpane)
+
+    f_init()
     $('.scaled').click(handler)
   }
 
+  f_init()
+  $('body').removeClass('faded')
   $('.scaled').click(handler)
 })
